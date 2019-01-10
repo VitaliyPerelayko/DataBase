4a7c0b7a7ab1ff79711a8dbef7e6895f69f2c992
VitaliyPerelayko <VitaliyPerelayko@users.noreply.github.com>
2019-01-10 20:25:16 +0300

    Task6

---
+++ Task6.sql
@@ -0,0 +1,31 @@
+select res.name as Company,
+       sum(exp.value) as Money
+from
+     listexpenses.expenses as exp,
+     listexpenses.receivers as res
+where
+    exp.receiver=res.num
+group by res.name;
+
+
+
+select paydate as Date,
+       sum(value) as Sum
+from
+     expenses
+where
+    paydate=(select paydate from expenses where value=(select max(value) from expenses));
+
+
+
+select distinct paydate as Date,
+       (select max(value) from expenses) as Payment
+from
+     expenses
+where
+    paydate=(select paydate from
+        (select max(val),money.paydate from
+                  (select sum(value) as val,paydate from expenses group by paydate) as money
+        )as bestDay
+            );
+
